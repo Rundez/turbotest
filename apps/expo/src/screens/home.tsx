aa172f039b96c5cb2238fb18960b94d62843744a
Rundez <m.ruud93@gmail.com>
2022-09-03 23:06:27 +0200

    test some queries and mutations

--- apps/expo/src/screens/home.tsx
+++ apps/expo/src/screens/home.tsx
@@ -1,8 +1,9 @@
-import { SafeAreaView, View, Text, Button } from "react-native";
+import { SafeAreaView, View, Text, Button, TextInput } from "react-native";
 import { FlashList } from "@shopify/flash-list";
 import type { inferProcedureOutput } from "@trpc/server";
 import type { AppRouter } from "@acme/api";
 import { trpc } from "../utils/trpc";
+import { useEffect, useState } from "react";
 
 const PostCard: React.FC<{
   post: inferProcedureOutput<AppRouter["post"]["all"]>[number];
@@ -16,11 +17,43 @@ const PostCard: React.FC<{
 };
 
 export const HomeScreen = () => {
-  const { data, error, isLoading, refetch} = trpc.post.all.useQuery();
+  const { data, refetch } = trpc.post.all.useQuery();
+  const { data: response, mutate, error } = trpc.post.add.useMutation();
+  const client = trpc.useContext();
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  console.log(error)
+
   return (
     <SafeAreaView>
       <View className="h-full w-full py-8 px-4">
         <Text className="text-5xl font-bold">Create T3 Turbo</Text>
+        <TextInput
+          onChangeText={setTitle}
+          value={title}
+          placeholder="Title..."
+          className="border border-gray-300 rounded-md p-2 w-full"
+        />
+        <TextInput
+          onChangeText={setContent}
+          value={content}
+          placeholder="Content..."
+          className="border border-gray-300 rounded-md p-2 w-full"
+        />
+
+
+        <Button
+          title="Add post"
+          onPress={() =>
+            mutate(
+              { content: content, title: title },
+              { onSuccess: () => client.post.all.invalidate() }
+            )
+          }
+        />
+
         <Button title="Refetch stuff" onPress={() => refetch()} />
 
         <FlashList
--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -3,10 +3,16 @@ import { z } from "zod";
 
 export const postRouter = t.router({
   all: t.procedure.query(({ ctx }) => {
-    console.log("finding all posts in server")
     return ctx.prisma.post.findMany();
   }),
   byId: t.procedure.input(z.string()).query(({ ctx, input }) => {
     return ctx.prisma.post.findFirst({ where: { id: input } });
   }),
+  add: t.procedure.input(z.object({
+    title: z.string().max(30),
+    content: z.string().max(50)
+  })).mutation(({ ctx, input }) => {
+    return ctx.prisma.post.create({ data: {title: input.title, content: input.content} });
+  }),
+
 });
